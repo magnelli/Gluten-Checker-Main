a1c78b79d94a3f9ca187f0f92b6fd78c4996c135
magnelli <magnelli@simmons.edu>
2018-07-25 16:45:46 -0700

    init commit

---
+++ GlutenChecker/GlutenChecker/GlutenChecker/GlutenChecker/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  GlutenChecker
+//
+//  Created by Marissa Magnelli on 7/25/18.
+//  Copyright © 2018 Marissa Magnelli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+
+//LoginViewController.swift
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
